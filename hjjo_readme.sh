6267b2bb3f4d441ae5b8ac482e777394d576689a
magmajo <hjcho0607@gmail.com>
2017-08-25 17:25:16 +0900

    add tag v0.0.1

--- hjjo_readme.sh
+++ hjjo_readme.sh
@@ -7,3 +7,20 @@ pod lib create [pod name]
 ## . spec project
 pod spec create [pod name]
 pod spec lint
+pod trunk register [E-MAIL] [NAME] -description=’[INFORMATION]’
+pod trunk push [podSpec file name]
+
+## . add/del tag
+##   - commit
+git commit -m "add cocoapods spec"
+
+##   - add tag
+git tag -a v0.0.1 -m "version 0.0.1"  -> annotated tag
+git tag v0.0.1 checksum -> lightweight tag (git log)
+##   - show log
+git log
+##   - show tag
+git tag
+##   - del tag
+git tag -d [version-name]
+git tag -d [checksum-name]
